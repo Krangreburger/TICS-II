5e472ab134a4e93a28cc9b971486fb9b09250c12
Flarenaster <felipe.larenasl@mail.udp.cl>
2015-04-29 03:18:42 -0300

    Aplicación Móvil

---
+++ App_movil/prototipoSGB/app/src/main/java/com/ticsii/prototiposgb/Home.java
@@ -0,0 +1,17 @@
+package com.ticsii.prototiposgb;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+/**
+ * Created by flarenaster on 28-04-15.
+ */
+public class Home extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        DBmanager helper= new DBmanager(this);
+    }
+}
